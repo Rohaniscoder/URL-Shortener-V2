fdfcd76a6bcd3464b5d35cd86c38408b19c00135
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-24 15:49:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            bot_tokens=BOT_TOKEN1,
+            bot_token=BOT_TOKEN1,
             plugins=dict(root="plugins"),
         )
 
