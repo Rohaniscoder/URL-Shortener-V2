bac68702d23d995532507ad90d027e10391646ed
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-02-17 02:10:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ BASE_SITE = os.environ.get("BASE_SITE", "tnlink.in")  # your shortener site doma
 # For Admin use
 CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
 CHANNEL_ID = (
-    [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
+    [int(i.strip()) for i in os.environ.get("CHANNEL_ID", "-100798404894").split(" ")]
     if os.environ.get("CHANNEL_ID")
     else []
 )
