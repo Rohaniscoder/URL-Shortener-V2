7585e956f9a38779082494a7e5ac9d9982095dd1
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-12-26 15:47:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,9 +15,9 @@ def is_enabled(value, default):
 
 
 # Mandatory variables for the bot to start
-API_ID = 1923471
-API_HASH = "fcdc178451cd234e63faefd38895c991"
-BOT_TOKEN = "5718312254:AAFEjjuPCvL9Z13ovnZQq0ghUq-6kJWP2R4"
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
 #BOT_TOKEN1 = "5477637816:AAFuRC-kgmPQJbRoZZe9d1gy7l98r0EHT2Q"
 ADMINS = (
     [int(i.strip()) for i in os.environ.get("ADMINS", 880087645).split(",")]
